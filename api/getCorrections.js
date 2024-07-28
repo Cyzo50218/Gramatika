670bc0e8ec892de20982ca688da42c970b9156f5
Cyzo50218 <155029018+Cyzo50218@users.noreply.github.com>
2024-07-29 07:15:20 +0800

    getCorrections.js

--- api/getCorrections.js
+++ api/getCorrections.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch'); // Ensure you have node-fetch installed
 
 module.exports = async (req, res) => {
   try {
-    const response = await fetch('path_to_corrections_file_or_api');
+    const response = await ('/api/corrections.json');
     if (!response.ok) throw new Error('Network response was not ok.');
     const corrections = await response.json();
     res.status(200).json(corrections);
