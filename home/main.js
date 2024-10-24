4469e6e0b886ebbe797ef717ea123a4c34ee6eab
Cyzo50218 <myfavoritemappingswar@gmail.com>
2024-10-24 18:43:11 +0800

    update main.js

--- home/main.js
+++ home/main.js
@@ -675,14 +675,14 @@ const moreNames = [
     let combinedEntry = words[j];
 
     // Check if the first word is a question word and the next word starts with a lowercase letter
-    if (j === 0 && /^(Ano|Sino|Saan|Kailan|kailan|Bakit|Papaano|Paano)/.test(words[j])) {
+    if (j === 0 && /^(Ano|Sino|Sino-sino|Kani-kanino|Kanino|Kailan|Saan|Saan-saan|Bakit|Paano|Ilan|Alin|Magkano|Gaano|Ano-ano|Papaano)/.test(words[j])) {
       
        console.log('QUESTION WORD DETECTED')
         
 changes = true;
 questionDetected = true;
     }
-    else if (j === 0 && /^[a-z]/.test(words[j]) && /^(ano|sino|saan|kailan|bakit|papaano|paano)/.test(words[j])) {
+    else if (j === 0 && /^[a-z]/.test(words[j]) &&/^(ano|sino|sino-sino|kani-kanino|kanino|kailan|saan|saan-saan|bakit|paano|ilan|alin|magkano|gaano|ano-ano|papaano)/.test(words[j])) {
       errorArray = [];
 firstletter = [];
 questionDetected = true;
